6ad8bff0922d15ca2a8d2509991774e5518c7e99
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-29 14:58:48 -0400

    add starting folders and files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello World</p>
       </header>
     </div>
   );
 }
-
-export default App;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+body {
+  font-size: 2rem;
+}
+
+`;
+
+export default GlobalStyles;
